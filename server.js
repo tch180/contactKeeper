0b4a2b50b13020a3855951183fed3c6f6dccce78
tch180 <thopstadredner@gmail.com>
2020-08-10 02:58:53 -0400

    forgot to uncomment a line for heroku update

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 //Serve Static assets in productions
-// if (process.env.NODE_ENV === 'production') {
-//   //set static folder
-//   app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   );
-// }
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
