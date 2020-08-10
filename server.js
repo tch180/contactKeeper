938624da49cbbd5be7d225b019abe3180fe6829c
tch180 <thopstadredner@gmail.com>
2020-08-10 02:52:31 -0400

    updated cluster, changed app.get

--- server.js
+++ server.js
@@ -15,15 +15,19 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-//Serve Static assets in productions
-if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static('client/build'));
+app.get('/', function (req, res) {
+  res.json(path.join(__dirname, 'public/index.html'));
+});
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
+//Serve Static assets in productions
+// if (process.env.NODE_ENV === 'production') {
+//   //set static folder
+//   app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   );
+// }
 
 const PORT = process.env.PORT || 5000;
 
